d99c5d96b3a4018e73788f28a0dfd43630ae9b90
Johnson Wong <cswong@hko.hksarg>
2020-08-20 14:33:39 +0800

    black format

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
-
 from pathlib import Path
-# Import MinIO library.
 from minio import Minio
 from minio.error import ResponseError, BucketAlreadyOwnedByYou, BucketAlreadyExists
 from progress import Progress
 
-mypath = '/mnt/d/Downloads/setaside'
+mypath = "/mnt/d/Downloads/setaside"
 
 endpoint = "localhost:9001"
 access_key = "minio"
@@ -18,7 +15,7 @@ filepath_full = list(Path(mypath).rglob("*"))
 filepath_full = [str(f) for f in filepath_full if not f.is_dir()]
 
 # rename file paths
-filepath = [f.replace(mypath, '').lstrip('/') for f in filepath_full]
+filepath = [f.replace(mypath, "").lstrip("/") for f in filepath_full]
 
 
 # Initialize minioClient with an endpoint and access/secret keys.
@@ -29,10 +26,13 @@ minioClient = Minio(
 progress = Progress()
 # Put an object with contents
 for filepath in filepath_full:
-    object_name = filepath.replace(mypath, '').lstrip('/')
+    object_name = filepath.replace(mypath, "").lstrip("/")
     try:
         minioClient.fput_object(
-            bucket_name=bucket_name, object_name=object_name, file_path=filepath, progress=progress
+            bucket_name=bucket_name,
+            object_name=object_name,
+            file_path=filepath,
+            progress=progress,
         )
     except ResponseError as err:
         print(err)
