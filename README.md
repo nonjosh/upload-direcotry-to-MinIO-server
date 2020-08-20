b0839ae97f615f0944c72ab78134c8c789891b5d
Johnson Wong <cswong@hko.hksarg>
2020-08-20 15:08:19 +0800

    add args support

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # upload direcotry to MinIO server
 
+## install dependency
+
+```bash
+pip install -r requirements.txt
+```
+
 ## Usage
 
+```bash
+$ python main.py --help
+usage: main.py [-h] -d DIR -e ENDPOINT --access_key ACCESS_KEY --secret_key SECRET_KEY -b BUCKET_NAME
 
+Upload a directory to MinIO server
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DIR, --dir DIR     path to directory, e.g. /mnt/d/Downloads/setaside
+  -e ENDPOINT, --endpoint ENDPOINT
+                        MinIO server endpoint, e.g. localhost:9001
+  --access_key ACCESS_KEY
+                        MinIO server access key
+  --secret_key SECRET_KEY
+                        MinIO server secret key
+  -b BUCKET_NAME, --bucket_name BUCKET_NAME
+                        MinIO server bucket_name, will create one if not exist
+```
+
+Examples
+
+```python
+python main.py --dir ~/Downloads --endpoint localhost:9001 --access_key minio --secret_key minio123 --bucket_name downloads
+```
 
 ## Reference
 
+- [https://docs.min.io/docs/python-client-quickstart-guide.html]
--- main.py
+++ main.py
@@ -2,37 +2,87 @@ from pathlib import Path
 from minio import Minio
 from minio.error import ResponseError, BucketAlreadyOwnedByYou, BucketAlreadyExists
 from progress import Progress
+import argparse
 
-mypath = "/mnt/d/Downloads/setaside"
+# main function
+if __name__ == "__main__":
 
-endpoint = "localhost:9001"
-access_key = "minio"
-secret_key = "minio123"
-bucket_name = "setaside"
+    # Define cmd line args
+    parser = argparse.ArgumentParser(
+        description="""Upload a directory to MinIO server"""
+    )
+    parser.add_argument(
+        "-d",
+        "--dir",
+        type=str,
+        help="path to directory, e.g. /mnt/d/Downloads/setaside",
+        required=True,
+    )
+    parser.add_argument(
+        "-e",
+        "--endpoint",
+        type=str,
+        help="MinIO server endpoint, e.g. localhost:9001",
+        required=True,
+    )
+    parser.add_argument(
+        "--access_key", type=str, help="MinIO server access key", required=True
+    )
+    parser.add_argument(
+        "--secret_key", type=str, help="MinIO server secret key", required=True,
+    )
+    parser.add_argument(
+        "-b",
+        "--bucket_name",
+        type=str,
+        help="MinIO server bucket_name, will create one if not exist",
+        required=True,
+    )
+    args = parser.parse_args()
 
-# get full paths for all files in the folder
-filepath_full = list(Path(mypath).rglob("*"))
-filepath_full = [str(f) for f in filepath_full if not f.is_dir()]
+    mypath = args.dir
 
-# rename file paths
-filepath = [f.replace(mypath, "").lstrip("/") for f in filepath_full]
+    endpoint = args.endpoint
+    access_key = args.access_key
+    secret_key = args.secret_key
+    bucket_name = args.bucket_name
 
+    print(args)
 
-# Initialize minioClient with an endpoint and access/secret keys.
-minioClient = Minio(
-    endpoint, access_key=access_key, secret_key=secret_key, secure=False,
-)
+    # get full paths for all files in the folder
+    filepath_full = list(Path(mypath).rglob("*"))
+    filepath_full = [str(f) for f in filepath_full if not f.is_dir()]
 
-progress = Progress()
-# Put an object with contents
-for filepath in filepath_full:
-    object_name = filepath.replace(mypath, "").lstrip("/")
+    # rename file paths
+    filepath = [f.replace(mypath, "").lstrip("/") for f in filepath_full]
+
+    print("{} files found".format(len(filepath_full)))
+
+    # Initialize minioClient with an endpoint and access/secret keys.
+    minioClient = Minio(
+        endpoint, access_key=access_key, secret_key=secret_key, secure=False,
+    )
+
+    # Make a bucket with the make_bucket API call.
     try:
-        minioClient.fput_object(
-            bucket_name=bucket_name,
-            object_name=object_name,
-            file_path=filepath,
-            progress=progress,
-        )
+        minioClient.make_bucket(bucket_name)
+    except BucketAlreadyOwnedByYou as err:
+        pass
+    except BucketAlreadyExists as err:
+        pass
     except ResponseError as err:
-        print(err)
+        raise
+
+    progress = Progress()
+    # Put an object with contents
+    for filepath in filepath_full:
+        object_name = filepath.replace(mypath, "").lstrip("/")
+        try:
+            minioClient.fput_object(
+                bucket_name=bucket_name,
+                object_name=object_name,
+                file_path=filepath,
+                progress=progress,
+            )
+        except ResponseError as err:
+            print(err)
